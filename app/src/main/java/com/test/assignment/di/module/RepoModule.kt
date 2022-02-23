29d14c77cc6bcbc2d32c5111c66f347d1ece8437
aslam <aslam.khan@skuad.io>
2022-02-23 17:24:09 +0530

    setup koin module

--- app/src/main/java/com/test/assignment/di/module/RepoModule.kt
+++ app/src/main/java/com/test/assignment/di/module/RepoModule.kt
@@ -1,2 +1,10 @@
 package com.test.assignment.di.module
 
+import com.test.assignment.data.repository.MainRepo
+import org.koin.dsl.module
+
+val repoModule = module {
+    single {
+        MainRepo(get())
+    }
+}
--- app/src/main/java/com/test/assignment/di/module/ViewModelModule.kt
+++ app/src/main/java/com/test/assignment/di/module/ViewModelModule.kt
@@ -1,2 +1,10 @@
 package com.test.assignment.di.module
 
+import com.test.assignment.ui.main.viewmodel.MainViewModel
+import org.koin.dsl.module
+import org.koin.android.viewmodel.dsl.viewModel
+val viewModelModule = module {
+    viewModel {
+        MainViewModel(get(),get())
+    }
+}
