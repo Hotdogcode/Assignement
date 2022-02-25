2fde7b167593c3adfc808613be2ab4d7201f0d3a
aslam <aslam.khan@skuad.io>
2022-02-25 10:03:01 +0530

    attach adapter to recycler view

--- app/src/main/java/com/test/assignment/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/test/assignment/ui/main/view/MainActivity.kt
@@ -2,11 +2,68 @@ package com.test.assignment.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.test.assignment.R
+import com.test.assignment.data.model.TrendingRepo
+import com.test.assignment.databinding.ActivityMainBinding
+import com.test.assignment.ui.main.adapter.MainAdapter
+import com.test.assignment.ui.main.viewmodel.MainViewModel
+import com.test.assignment.utils.Status
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel : MainViewModel by viewModel()
+    private lateinit var adapter: MainAdapter
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        setupUI()
+        setupObserver()
     }
+
+    private fun setupUI() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MainAdapter()
+        binding.recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                binding.recyclerView.context,
+                (binding.recyclerView.layoutManager as LinearLayoutManager).orientation
+            )
+        )
+        binding.recyclerView.adapter = adapter
+    }
+    private fun setupObserver() {
+        mainViewModel.repos.observe(this, Observer {
+            when (it.status) {
+                Status.SUCCESS -> {
+                    binding.progressBar.visibility = View.GONE
+                    it.data?.let { users -> renderList(users) }
+                    binding.recyclerView.visibility = View.VISIBLE
+                }
+                Status.LOADING -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                    binding.recyclerView.visibility = View.GONE
+                }
+                Status.ERROR -> {
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+    }
+
+    private fun renderList(repos: List<TrendingRepo>) {
+        adapter.setItems(repos)
+    }
+
+
 }
